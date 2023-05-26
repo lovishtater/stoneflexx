629853897998ef21077b0a01d1fb45ec4528a50c
lovishtater08 <lovish.t@source.one>
2023-05-26 17:57:14 +0530

    Added All pages

--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Products = () => {
-  return <div>Products</div>;
+  const _id = useParams();
+  const { products } = sessionStorage.getItem('products');
+  const currentProduct = products.find(product => product._id === _id);
+    const [productDetails, setProductDetails] = React.useState({
+        activeTag: null,
+        quantity: 1, 
+    });
+
+  return <div className="flex flex-col items-center justify-center w-full h-full">
+    { currentProduct ? (
+        <div className="flex flex-col items-center justify-center w-full h-full">
 };
 
 export default Products;
